ec7573b037be68642600e2ee6dfd0bac8edafb7e
suwitna <44050482+suwitna@users.noreply.github.com>
2020-03-29 18:57:26 +0700

    Version viewer

--- Covid19Map/Covid19Map/App.xaml.cs
+++ Covid19Map/Covid19Map/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Covid19Map.Views;
+﻿using Covid19Map.View;
+using Covid19Map.Views;
 using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -11,14 +12,20 @@ namespace Covid19Map
         {
             NavigationPage.SetHasNavigationBar(this, false);
             InitializeComponent();
-            MainPage = new NavigationPage(new LoginPage());
+            //Verion Login
+            //MainPage = new NavigationPage(new LoginPage());
+
+            //Verion report location
             //MainPage = new NavigationPage(new LocationTrackerPage());
+
+            //version Nakhonphanom
+            MainPage = new NavigationPage(new CovidAtNKPPage());
             /*
             InitializeComponent();
 
             MainPage = new MainPage();
             */
-           
+
         }
 
         protected override void OnStart()
---
+++ Covid19Map/Covid19Map/View/CovidAtNKPPage.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+using Xamarin.Forms.Xaml;
+
+namespace Covid19Map.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CovidAtNKPPage : ContentPage
+    {
+        FirebaseHelper firebaseHelper = new FirebaseHelper();
+        double zoomMeters = 5000;
+        double latitude = 17.3773698;
+        double longitude = 104.7608508;
+
+        public CovidAtNKPPage()
+        {
+            InitializeComponent();
+            btnRefresh.Clicked += BtnRefresh_Clicked;
+        }
+
+        private async void BtnRefresh_Clicked(object sender, EventArgs e)
+        {
+            latitude = 17.3773698;
+            longitude = 104.7608508;
+            await RetreiveLocation();
+        }
+
+        protected async override void OnAppearing()
+        {
+
+            base.OnAppearing();
+            await RetreiveLocation();
+        }
+
+        private async Task RetreiveLocation()
+        {
+
+            if (MyMap.VisibleRegion != null)
+            {
+                Distance level = MyMap.VisibleRegion.Radius;
+                zoomMeters = level.Meters;
+
+                latitude = MyMap.VisibleRegion.Center.Latitude;
+                longitude = MyMap.VisibleRegion.Center.Longitude;
+            }
+            else
+            {
+                zoomMeters = 5000;
+                latitude = 17.3773698;
+                longitude = 104.7608508;
+            }
+            MyMap.Pins.Clear();
+
+            MyMap.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(latitude, longitude)
+                        , Distance.FromMeters(zoomMeters)));
+
+            var allPersons = await firebaseHelper.GetAllCovidMap();
+            foreach (var item in allPersons)
+            {
+                Position pos = new Position(Convert.ToDouble(item.Latitude), Convert.ToDouble(item.Longitude));
+
+                CustomPin pin = new CustomPin
+                {
+                    Type = PinType.SearchResult,
+                    Position = pos,
+                    Label = item.PinLabel,
+                    Address = item.PinAddress,
+                };
+                MyMap.Pins.Add(pin);
+            }
+ 
+        }
+
+        protected override bool OnBackButtonPressed()
+        {
+            Device.BeginInvokeOnMainThread(async () => {
+                var result = await DisplayAlert("ออกจากแอพพลิเคชั่น", "ท่านกำลังออกจากระบบ โปรดยืนยัน?", "ตกลง", "ยกเลิก");
+                if (result)
+                {
+                    // await this.Navigation.PopAsync(); // or anything else
+                    if (Device.RuntimePlatform == Device.Android)
+                    {
+                        Android.OS.Process.KillProcess(Android.OS.Process.MyPid());
+                    }
+                }
+            });
+
+            return true;
+        }
+    }
+}
